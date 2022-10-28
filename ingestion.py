72d1437f19ae5e313f378d74b15d251450d836b2
cecilegltslmcs <cecile.guillot08@protonmail.com>
2022-10-28 15:22:44 +0200

    created function for gathering

--- ingestion.py
+++ ingestion.py
@@ -1,9 +1,12 @@
 import requests
 import json
 
-url = "https://odre.opendatasoft.com/api/v2/catalog/datasets/eco2mix-regional-cons-def/exports/json"
-response = requests.get(url)
+def gathering_data(url):
+  response = requests.get(url)
+  data = response.json()
+  with open('data/data.json', 'w') as f:
+    json.dump(data, f)
 
-data = response.json()
-with open('data.json', 'w') as f:
-  json.dump(data, f)
+if __name__ == "__main__":
+  url = "https://odre.opendatasoft.com/api/v2/catalog/datasets/eco2mix-regional-cons-def/exports/json"
+  gathering_data(url)
