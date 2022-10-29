01b76b007b3eca877c1b1cbca04fe9c3f661fb1a
cecilegltslmcs <cecile.guillot08@protonmail.com>
2022-10-29 18:54:31 +0200

    Added feedback message in main function

--- ingestion.py
+++ ingestion.py
@@ -7,11 +7,11 @@ def gathering_data(url):
     data = response.json()
   except:
     print('Wrong URL')
-  print("Saving in progress...")
   with open('data/energy_data.json', 'w') as f:
     json.dump(data, f)
-  print("Saving termined!")
 
 if __name__ == "__main__":
+  print("Gathering data in progress...")
   url = "https://odre.opendatasoft.com/api/v2/catalog/datasets/eco2mix-regional-cons-def/exports/json"
   gathering_data(url)
+  print("Data obtained!")
