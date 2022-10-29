59e38a31a218fc2bbc725804fd3e349854e4bc8d
cecilegltslmcs <cecile.guillot08@protonmail.com>
2022-10-29 18:49:34 +0200

    Adding feedback message

--- ingestion.py
+++ ingestion.py
@@ -2,10 +2,15 @@ import requests
 import json
 
 def gathering_data(url):
-  response = requests.get(url)
-  data = response.json()
-  with open('data/data.json', 'w') as f:
+  try:
+    response = requests.get(url)
+    data = response.json()
+  except:
+    print('Wrong URL')
+  print("Saving in progress...")
+  with open('data/energy_data.json', 'w') as f:
     json.dump(data, f)
+  print("Saving termined!")
 
 if __name__ == "__main__":
   url = "https://odre.opendatasoft.com/api/v2/catalog/datasets/eco2mix-regional-cons-def/exports/json"
