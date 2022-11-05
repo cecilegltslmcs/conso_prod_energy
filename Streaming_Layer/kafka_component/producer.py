b87d059c2fe810555dceea5b229d232098bda7b6
cecilegltslmcs <cecile.guillot08@protonmail.com>
2022-11-05 20:37:43 +0100

    Modification on kafka producer

--- Streaming_Layer/kafka_component/producer.py
+++ Streaming_Layer/kafka_component/producer.py
@@ -4,23 +4,16 @@ from kafka import KafkaProducer
 import json
 
 def get_data(url):
-    mytime = strftime('%Y-%m-%d %H:%M:%S', localtime())
-
     resp = requests.get(url)
-
     data = resp.json()
-    data['timestamp'] = mytime
-
     return data
 
 def get_producer(ip, port=9092):
     broker = f"{ip}:{port}"
-    print("connecting to ", broker)
-
+    print("connecting to", broker)
     producer = KafkaProducer(
                 bootstrap_servers = [broker],
                 value_serializer = lambda x: json.dumps(x).encode('utf-8'))
-
     return producer
 
 def send_records_to_kafka(producer, topic, records):
@@ -36,6 +29,6 @@ if __name__ == "__main__":
 
     while True:
         data = get_data(url)
-        send_records_to_kafka(producer, topic, data['records'])
+        send_records_to_kafka(producer, topic, data)
         print("waiting 15 minutes for next records")
         time.sleep(900)
