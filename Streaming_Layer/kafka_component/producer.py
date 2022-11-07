2581c8b7cb6c0a9dc38185a86a874aeba33ff061
Cécile Guillot <64648386+cecilegltslmcs@users.noreply.github.com>
2022-11-07 14:13:49 +0100

    Update producer.py

--- Streaming_Layer/kafka_component/producer.py
+++ Streaming_Layer/kafka_component/producer.py
@@ -1,25 +1,4 @@
-import requests
-import time
-from kafka import KafkaProducer
-import json
-
-def get_data(url):
-    resp = requests.get(url)
-    data = resp.json()
-    return data
-
-def get_producer(ip, port=9092):
-    broker = f"{ip}:{port}"
-    print("connecting to", broker)
-    producer = KafkaProducer(
-                bootstrap_servers = [broker],
-                value_serializer = lambda x: json.dumps(x).encode('utf-8'))
-    return producer
-
-def send_records_to_kafka(producer, topic, records):
-    for record in records:
-        print("sending records")
-        producer.send(topic, record)
+from module_kafka import *
 
 if __name__ == "__main__":
     ip = "localhost"
