dbe8387586734532bf439b619d97f54a1c3db264
Cécile Guillot <64648386+cecilegltslmcs@users.noreply.github.com>
2022-11-07 17:45:54 +0100

    Update test_ingestion_module.py

--- Batch_Layer/Ingestion/test_ingestion_module.py
+++ Batch_Layer/Ingestion/test_ingestion_module.py
@@ -1,17 +1,20 @@
 from ingestion import *
-import unittest
+import pandas as pd
+import pytest
 
-def test_collecting_data(output):
+def test_collecting_data():
+  output = collecting_data(url)
+  assert output == "Data collected from API successfully."
   
 def test_opening_data():
   output = opening_data(path)
-  assert output == type
+  assert output == type(json)
 
 def test_cleaning_data():
   output1, output2, output3 = cleaning_data(data)
-  assert output1, output2, output3 == type(dataframe)
+  assert output1, output2, output3 == type(pd.dataframe)
 
 def test_sending_database():
   output = sending_database()
-  assert output ==
+  assert output == "Transfer.s finished!"
   
