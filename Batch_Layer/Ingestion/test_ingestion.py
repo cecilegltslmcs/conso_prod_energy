223db5f8f48f038cf566fe3a4efab521a36615a2
Cécile Guillot <64648386+cecilegltslmcs@users.noreply.github.com>
2022-11-07 13:45:42 +0100

    Create test_ingestion.py

---
+++ Batch_Layer/Ingestion/test_ingestion.py
@@ -0,0 +1,17 @@
+from ingestion import *
+import pytest
+
+def test_collecting_data(output):
+  
+def test_opening_data():
+  output = opening_data(path)
+  assert output == type
+
+def test_cleaning_data():
+  output1, output2, output3 = cleaning_data(data)
+  assert output1, output2, output3 == type(dataframe)
+
+def test_sending_database():
+  output = sending_database()
+  assert output ==
+  
