91f16526c67fbaf63ff241120c3bf3b583f96d47
Cécile Guillot <64648386+cecilegltslmcs@users.noreply.github.com>
2022-11-08 11:34:47 +0100

    Update test_module_ingestion.py

--- src/batch_layer/tests/test_module_ingestion.py
+++ src/batch_layer/tests/test_module_ingestion.py
@@ -10,4 +10,16 @@ def test_opening_data():
   df = opening_data(path)
   assert df == type(pd.DataFrame)
 
+def test_parsing_data():
+  output1, output2, output3 = parsing_data(df)
+  assert output1, output2, output3 == type(pd.DataFrame)
+
+def test_processing_data():
+  output1 = processing_data(df)
+  assert output1 == type(pd.DataFrame)
+  
+def test_sending_to_database():
+  output1 = sending_to_database()
+  assert output1 == "Transfert(s) finished!"
+
   
