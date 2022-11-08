eb2bd418d56e18eebb0b9c0f9637127de8864489
Cécile Guillot <64648386+cecilegltslmcs@users.noreply.github.com>
2022-11-08 11:06:02 +0100

    Update test_ingestion_module.py

--- src/batch_layer/tests/test_ingestion_module.py
+++ src/batch_layer/tests/test_ingestion_module.py
@@ -2,4 +2,11 @@ from batch_layer.packages.modules.module_ingestion import *
 import pandas as pd
 import pytest
 
+def test_collecting_data():
+  r = requests.get(f'url')
+  assert r.status_code == 200
+
+def test_opening_data():
+  
+
   
