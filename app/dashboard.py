bf067d10291f2d07f1f25c8579f687aa003d6b1a
Cecile Guillot <cecile.guillot08@protonmail.com>
2022-12-14 18:18:22 +0100

    Add tab in dashboard

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,14 +1,11 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from flask import Flask
+from dash import Dash, dcc, html
+from dash.dependencies import Input, Output
 import os
 import plotly.io as pio
-import plotly.express as px
 from pymongo import MongoClient
 import packages.authentification as auth
+from packages.module_dashboard import *
 import pandas as pd
-import requests
 
 user = auth.mongodb_user
 password = auth.mongodb_password
@@ -17,7 +14,7 @@ port = auth.mongodb_port
 uri = f"mongodb://{user}:{password}@{host}:{port}"
 
 pio.templates.default = "seaborn"
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 debug = False if os.environ["DASH_DEBUG_MODE"] == "False" else True
 
@@ -29,14 +26,6 @@ def get_data():
     items = db['streaming_data'].find()
     items = list(items)
     return items
-
-def loading_geojson(url):
-    try:
-        response = requests.get(url)
-        response = response.json()
-    except:
-        print("Wrong URL")
-    return response
 
 try:
     client = init_connection()
@@ -50,27 +39,40 @@ df = pd.DataFrame(items)
 url = "https://www.data.gouv.fr/fr/datasets/r/d993e112-848f-4446-b93b-0a9d5997c4a4"
 region_geojson = loading_geojson(url)
 
-app = dash.Dash(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title = "Dashboard"
+app.title = "Dashboard de la consommation et de la production d'énergie"
 
-fig = px.choropleth_mapbox(df, geojson=region_geojson,
-                               featureidkey="properties.reg",
-                               locations='code_insee_region',
-                               color=df.consommation,
-                               color_continuous_scale="rdylbu_r",
-                               mapbox_style="carto-positron",
-                               zoom=4, center = {"lat": 46.2276, "lon": 2.21},
-                               opacity=0.6)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig_conso = choropleth_map(df, region_geojson, df.consommation)
+fig_prod = choropleth_map(df, region_geojson, df.production)
+stack_prod = stack_chart(df)
+
 
 app.layout = html.Div([
     html.Div([
-        dcc.Markdown("""# Tableau de bord de la consommation et de la production d'énergie en France en temps réel
-    Cette application permet de visualiser les données de consommation et de production
-    d'énergie région par région. Les données présentées sont actualisées toutes les 15 minutes.
-    Source : *[Open Data Réseaux-Energies](https://opendata.reseaux-energies.fr/)*""")]),
-        dcc.Graph(figure=fig)
+        dcc.H4("""# Tableau de bord de la consommation et de la production d'énergie en France en temps réel
+    Cette application permet de visualiser les données de consommation et de production d'énergie région par région. Les données présentées sont actualisées toutes les 15 minutes.
+    Source : [Open Data Réseaux-Energies](https://opendata.reseaux-energies.fr/)""")]),
+        dcc.Tabs(id="tabs", value='tab-1', children=[
+            dcc.Tab(label="Consommation", value="tab-1"),
+            dcc.Tab(label="Production", value="tab-2"),
+        ]),
+        html.Div(id="tabs-content")
+        ])
+
+@app.callback(Output('tabs-content', 'children'),
+              Input('tabs', 'value'))
+def render_content(tab):
+    if tab == "tab-1":
+        return html.Div([
+            html.H3("Données de consommation en temps réel (actualisation toutes les 15 minutes)"),
+            dcc.Graph(figure=fig_conso)
+        ])
+    elif tab == "tab-2":
+        return html.Div([
+            html.H3("Données de production en temps réel (actualisation toutes les 15 minutes)"),
+            dcc.Graph(figure=fig_prod),
+            dcc.Graph(figure=stack_prod)
         ])
 
 if __name__ == "__main__":
