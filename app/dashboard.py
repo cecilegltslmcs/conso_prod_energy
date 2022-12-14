2d8ff84a0c521d7888733b01848d126e47dd92d3
Cecile Guillot <cecile.guillot08@protonmail.com>
2022-12-14 17:05:44 +0100

    Initial commit Dashboard v2

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,12 +1,14 @@
-from packages.module_dashboard import *
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from flask import Flask
+import os
+import plotly.io as pio
+import plotly.express as px
 from pymongo import MongoClient
 import packages.authentification as auth
 import pandas as pd
-import streamlit as st
-import time
-
-url = "https://www.data.gouv.fr/fr/datasets/r/d993e112-848f-4446-b93b-0a9d5997c4a4"
-region_geojson = loading_geojson(url)
+import requests
 
 user = auth.mongodb_user
 password = auth.mongodb_password
@@ -14,9 +16,27 @@ host = auth.mongodb_host
 port = auth.mongodb_port
 uri = f"mongodb://{user}:{password}@{host}:{port}"
 
-@st.experimental_singleton
+pio.templates.default = "seaborn"
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+debug = False if os.environ["DASH_DEBUG_MODE"] == "False" else True
+
 def init_connection():
     return MongoClient(uri)
+
+def get_data():
+    db = client['electricity_prod']
+    items = db['streaming_data'].find()
+    items = list(items)
+    return items
+
+def loading_geojson(url):
+    try:
+        response = requests.get(url)
+        response = response.json()
+    except:
+        print("Wrong URL")
+    return response
 
 try:
     client = init_connection()
@@ -24,55 +44,34 @@ try:
 except:
     print("Connection error")
 
-@st.experimental_memo(ttl=1)
-def get_data():
-    db = client['electricity_prod']
-    items = db['streaming_data'].find()
-    items = list(items)
-    return items
+items = get_data()
+df = pd.DataFrame(items)
 
-st.title("Tableau de bord de la consommation et de production d'électricité en France en temps réel")
-option = st.sidebar.selectbox("Choississez une page.", ('Accueil', 'Général', 'Consommation', 'Production totale', 'Répartition production'))
+url = "https://www.data.gouv.fr/fr/datasets/r/d993e112-848f-4446-b93b-0a9d5997c4a4"
+region_geojson = loading_geojson(url)
 
-if option == "Accueil":
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.write(' ')
-    with col2:
-        st.image("images/logo-odre.svg", width=75)
-    with col3:
-        st.write(' ')
-    st.header("Bienvenue sur cet outil qui vous permet de consulter les consommations et productions d'électricité région par région en France en temps réel.\
-               Sélectionnez une analyse sur le menu de gauche pour continuer.")
-    st.image("images/power-lines.jpg")
-    st.write("Réalisé par Cécile Guillot - Données provenant de l'API Open Data Réseaux-Energies")
+app = dash.Dash(__name__)
+server = app.server
+app.title = "Dashboard"
 
-placeholder = st.empty()
-    
-while True:
-    items = get_data()
-    
-    with placeholder.container():
-        df = pd.DataFrame(items)
+fig = px.choropleth_mapbox(df, geojson=region_geojson,
+                               featureidkey="properties.reg",
+                               locations='code_insee_region',
+                               color=df.consommation,
+                               color_continuous_scale="rdylbu_r",
+                               mapbox_style="carto-positron",
+                               zoom=4, center = {"lat": 46.2276, "lon": 2.21},
+                               opacity=0.6)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-        if option == "Général":
-            counts = len(df.index)
-            st.write("Nombre d'entrées : ", counts)
-            st.write(df)
+app.layout = html.Div([
+    html.Div([
+        dcc.Markdown("""# Tableau de bord de la consommation et de la production d'énergie en France en temps réel
+    Cette application permet de visualiser les données de consommation et de production
+    d'énergie région par région. Les données présentées sont actualisées toutes les 15 minutes.
+    Source : *[Open Data Réseaux-Energies](https://opendata.reseaux-energies.fr/)*""")]),
+        dcc.Graph(figure=fig)
+        ])
 
-        if option == "Consommation":
-            st.header("Carte de la consommation d'énergie en temps réel (mise à jour toutes les 15 minutes)")
-            map_conso = choropleth_map(df, region_geojson, df.consommation)
-            st.write(map_conso)
-        
-        if option == "Production totale":
-            st.header("Carte de la production d'énergie en temps réel (mise à jour toutes les 15 minutes)")
-            map_prod = choropleth_map(df, region_geojson, df.production)
-            st.write(map_prod)
-        
-        if option == "Répartition production":
-            st.header("Graphique de la répartition de la production d'énergie par type (mise à jour toutes les 15 minutes)")
-            chart = stack_chart(df)
-            st.write(chart)
-
-        time.sleep(1)
+if __name__ == "__main__":
+    app.run_server(host="0.0.0.0", port="8050", debug=debug)
